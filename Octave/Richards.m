00dba06cde25c495ccbb2980a549b12657be998a
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-02-01 16:21:33 +0100

    “Richards.m”: removed all code relevant for plotting; added code for saving model data for future use.

--- Octave/Richards.m
+++ Octave/Richards.m
@@ -10,6 +10,7 @@ parset = "Lt2";
 parset = "Uu";
 parset = "Celia";
 source("van_Genuchten_parameters.m");
+
 # Set model parameters:
 delta_t = 10; # distance between time levels in s
 delta_z = 0.3; # vertical distance between spatial levels in cm
@@ -72,121 +73,16 @@ for timestep = 2:length(t)
   H_mat(:,timestep) = H_n;
   theta_mat(:,timestep) = theta_n;
 endfor
-
-plot(H_mat(1,:))
-# # Set graphics toolkit:
- graphics_toolkit("gnuplot");
-# # Set destination directory for saving data:
-# data_dir = "../Daten/Probelaeufe/";
-# # Set names of files in which to save and from which to read data:
-# H_mat_file = cat(2, data_dir, "H_mat.csv");
-# theta_mat_file = cat(2, data_dir, "theta_mat.csv");
-# # Set default file type for saving data:
-# save_default_options("-text");
-# 
-# # Save data #
-# save (H_mat_file, "H_mat");
-# save (theta_mat_file, "theta_mat");
-# 
-# # Read data #
-# H_mat = dlmread(H_mat_file," ",5,0);
-# theta_mat = dlmread(theta_mat_file," ",5,0);
-# 
-# # Plot H:
-# close all;
-# figurehandle = figure("papertype",
-# 		  "a4",
-# 		  "paperorientation",
-# 		  "landscape",
-# 		  "paperunits",
-# 		  "centimeters",
-# 		  "paperposition",
-# 		  [2 2 17 25.6],
-# 		  "visible",
-# 		  # "off");
-# 		  "on");
-# xlabel("Timestep");
-# ylabel("Node");
-# zlabel("Pressure head [cm]");
-# axis([0;
-#       length(t) + 1;
-#       0;
-#       length(z) + 1;
-#       min(min(H_mat)) - 1 * (10 ^ -9);
-#       max(max(H_mat)) + 1 * (10 ^ -7)],
-#      "tic",
-#      "label");
-# grid ("on");
-# view(45,
-#      35);
-# color = ["k"];
-# linestyle = "-";
-# linewidth = 2;
-# # Create plots using for-loop:
-# # for row = [1, median(1:rows(H_mat)), rows(H_mat)] # print only a selection of nodes
-#   for row = 1:rows(H_mat)
-#   line(1:length(t),
-#        (1:length(z))(row),
-#        # H_mat(row,:).', # transposing seems to result in plotting the same line mutlitple times
-#        H_mat(row,:),
-#        "linewidth",
-#        linewidth,
-#        "linestyle",
-#        linestyle,
-#        "color",
-#        color)
-# endfor
-# system("rm -vf ../Grafiken/H_mat_3D_plot.pdf");
-# print(figurehandle,
-#       "../Grafiken/H_mat_3D_plot.pdf");
-# 
-# system("mupdf -r 66 ../Grafiken/H_mat_3D_plot.pdf &");
-# 
-# # Plot theta:
-# close all;
-# figurehandle = figure("papertype",
-# 		  "a4",
-# 		  "paperorientation",
-# 		  "landscape",
-# 		  "paperunits",
-# 		  "centimeters",
-# 		  "paperposition",
-# 		  [2 2 17 25.6],
-# 		  "visible",
-# 		  # "off");
-# 		  "on");
-# xlabel("Timestep");
-# ylabel("Node");
-# zlabel("Pressure head [cm]");
-# axis([0;
-#       length(t) + 1;
-#       0;
-#       length(z) + 1;
-#       min(min(theta_mat)) - 1 * (10 ^ -9);
-#       max(max(theta_mat)) + 1 * (10 ^ -7)],
-#      "tic",
-#      "label");
-# grid ("on");
-# view(45,
-#      35);
-# color = ["k"];
-# linestyle = "-";
-# linewidth = 2;
-# # Create plots using for-loop:
-# for row = [1, median(1:rows(theta_mat)), rows(theta_mat)] # print only a selection of nodes
-#   # for row = 1:rows(theta_mat)
-#   line(1:length(t),
-#        (1:length(z))(row),
-#        # theta_mat(row,:).', # transposing seems to result in plotting the same line mutlitple times
-#        theta_mat(row,:),
-#        "linewidth",
-#        linewidth,
-#        "linestyle",
-#        linestyle,
-#        "color",
-#        color)
-# endfor
-# system("rm -vf ../Grafiken/theta_mat_3D_plot.pdf");
-# print(figurehandle,
-#       "../Grafiken/theta_mat_3D_plot.pdf");
-# system("mupdf -r 66 ../Grafiken/theta_mat_3D_plot.pdf &");
+#################
+## Saving data ##
+#################
+# Set destination directory for saving data:
+data_dir = "../Daten/Probelaeufe/";
+# Set names of files in which to save and from which to read data:
+H_mat_file = cat(2, data_dir, "H_mat.csv");
+theta_mat_file = cat(2, data_dir, "theta_mat.csv");
+# Set default file type for saving data:
+save_default_options("-text");
+# Save data #
+save (H_mat_file, "H_mat");
+save (theta_mat_file, "theta_mat");
