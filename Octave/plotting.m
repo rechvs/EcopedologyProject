004f80e79bbe9499490d643af7f720f5f072b8c1
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-02-01 19:08:05 +0100

    “plotting.m”: inserted sections for plotting all spatial and all time levels separately and saving each plot in a separate file.

--- Octave/plotting.m
+++ Octave/plotting.m
@@ -1,3 +1,5 @@
+## Clear workspace:
+clear();
 ## Set directory from which to read data:
 data_dir = "../Daten/Probelaeufe/";
 ## Set names of files from which to read data:
@@ -8,13 +10,69 @@ H_mat = dlmread(H_mat_file," ",5,0);
 theta_mat = dlmread(theta_mat_file," ",5,0);
 ## Set graphics toolkit:
 graphics_toolkit("gnuplot");
-## Create 2D plots:
-close all;
-plot(H_mat(1,:))
-plot(H_mat(median(1:rows(H_mat)),:))
+
+##################
+## Plot H in 2D ##
+##################
+## Plot H for a single spatial level over all time levels:
+for row = 1:rows(H_mat)
+  close all;
+  y=H_mat(row,:);
+  x=1:length(y);
+  figurehandle = figure("papertype",
+		    "a4",
+		    "paperorientation",
+		    "landscape",
+		    "paperunits",
+		    "centimeters",
+		    "paperposition",
+		    [2 2 17 25.6],
+		    "visible",
+		    "off");
+  ## "on");
+  line(x,y,
+       "linewidth",
+       3);
+  xlabel("Time level");
+  ylabel("Pressure head [cm]");
+  title(["Spatial level: ",num2str(row)]);
+  grid ("on");
+  filename=(["../Grafiken/H_mat_2D_plots/spatial_levels/spatial_level_",sprintf("%03d",row),".pdf"]);
+  print(figurehandle,
+        filename);
+endfor
+
+## Plot H for a single time level over all spatial levels:
+for col = 1:columns(H_mat)
+  close all;
+  y=H_mat(:,col);
+  x=1:length(y);
+  figurehandle = figure("papertype",
+		    "a4",
+		    "paperorientation",
+		    "landscape",
+		    "paperunits",
+		    "centimeters",
+		    "paperposition",
+		    [2 2 17 25.6],
+		    "visible",
+		    "off");
+  ## "on");
+  line(x,y,
+       "linewidth",
+       3);
+  xlabel("Spatial level");
+  ylabel("Pressure head [cm]");
+  title(["Time level: ",num2str(col)]);
+  grid ("on");
+  filename=(["../Grafiken/H_mat_2D_plots/time_levels/time_level_",sprintf("%03d",col),".pdf"]);
+  print(figurehandle,
+        filename);
+endfor
+  
 
 ## Plot H in 3D:
-close all;
+## close all;
 figurehandle = figure("papertype",
 		  "a4",
 		  "paperorientation",
@@ -24,8 +82,8 @@ figurehandle = figure("papertype",
 		  "paperposition",
 		  [2 2 17 25.6],
 		  "visible",
-		  ## "off");
-		  "on");
+		  "off");
+		  ## "on");
 xlabel("Timestep");
 ylabel("Node");
 zlabel("Pressure head [cm]");
@@ -46,8 +104,8 @@ color = ["k"];
 linestyle = "-";
 linewidth = 2;
 ## Create plots using for-loop:
-for row = [1, median(1:rows(H_mat)), rows(H_mat)] ## print only a selection of nodes
-  ## for row = 1:rows(H_mat)
+## for row = [1, median(1:rows(H_mat)), rows(H_mat)] ## print only a selection of nodes
+  for row = 1:rows(H_mat)
   line(1:length(t),
        (1:length(z))(row),
        ## H_mat(row,:).', ## transposing seems to result in plotting the same line mutlitple times
@@ -59,6 +117,7 @@ for row = [1, median(1:rows(H_mat)), rows(H_mat)] ## print only a selection of n
        "color",
        color)
 endfor
+
 system("rm -vf ../Grafiken/H_mat_3D_plot.pdf");
 print(figurehandle,
       "../Grafiken/H_mat_3D_plot.pdf");
