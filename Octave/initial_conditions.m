2238e3d5a518db84f669fbf78830f41763585017
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-02-08 11:22:45 +0100

    “initial_conditions.m”: added second initial condition option.

--- Octave/initial_conditions.m
+++ Octave/initial_conditions.m
@@ -5,6 +5,10 @@ if (init_con == 1)
 ### Description: linear change using boundary conditions
   init_con_string = "linspace(H_bot,H_top,length(z))'";
   H_0 = eval(init_con_string);
+elseif (init_con == 2)
+### Description: linear change from -60 cm at bottom to -30 cm at top
+  init_con_string = "linspace(-60,-30,length(z))'";
+  H_0 = eval(init_con_string);
 else ## fallback 
   error("%s\n","unknown value of \"init_con\".")
 endif
