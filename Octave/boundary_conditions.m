56b8adf9ff73ad0364b11176617296be71a65b8e
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-03-17 18:08:27 +0100

    “Octave/Richards.m”: added a variable to switch between scenarios “constant pressure head” and “no flux”.

--- Octave/boundary_conditions.m
+++ Octave/boundary_conditions.m
@@ -6,17 +6,21 @@ if (bound_con == 1)
   H_bot = 0;
   H_top = -z_final;
 elseif (bound_con == 2)
+### Description: free water at bottom, wet top
+  H_bot = 0;
+  H_top = -10;
+elseif (bound_con == 3)
+### Description: free water at bottom, moderately dry top
+  H_bot = 0;
+  H_top = -35;
+elseif (bound_con == 4)
 ### Description: free water at bottom, ca. FC at top
   H_bot = 0;
   H_top = -60;
-elseif (bound_con == 3)
+elseif (bound_con == 5)
 ### Description: free water at bottom, dry top
   H_bot = 0;
   H_top = -300;
-elseif (bound_con == 4)
-### Description: dry bottom, wet top
-  H_bot = -60;
-  H_top = 0;
 else ## fallback 
   error("%s\n","unknown value of \"bound_con\".")
 endif
