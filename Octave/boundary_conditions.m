1f0c66eebaa114569933008da363118ecbb41d5e
Renke Christian von Seggern <r.vonseggern@stud.uni-goettingen.de>
2017-03-19 16:27:35 +0100

    “Octave/boundary_conditions.m”: reordered boundary conditions settings; this version was used to create figures on 2017-03-19.

--- Octave/boundary_conditions.m
+++ Octave/boundary_conditions.m
@@ -10,17 +10,17 @@ elseif (bound_con == 2)
   H_bot = 0;
   H_top = -10;
 elseif (bound_con == 3)
-### Description: free water at bottom, moderately dry top
-  H_bot = 0;
-  H_top = -35;
-elseif (bound_con == 4)
-### Description: free water at bottom, ca. FC at top
-  H_bot = 0;
-  H_top = -60;
-elseif (bound_con == 5)
 ### Description: free water at bottom, dry top
   H_bot = 0;
   H_top = -300;
+elseif (bound_con == 4)
+### Description: free water at bottom, moderately dry top
+  H_bot = 0;
+  H_top = -35;
+elseif (bound_con == 5)
+### Description: free water at bottom, ca. FC at top
+  H_bot = 0;
+  H_top = -60;
 else ## fallback 
   error("%s\n","unknown value of \"bound_con\".")
 endif
